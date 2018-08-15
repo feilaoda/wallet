ae0f11a80ca3d3b4b930d9763df08b22b367acaf
marco <lxgaisqq@163.com>
2018-08-15 15:55:58 +0800

    Fixed #398，添加资产等待框不弹出来问题

--- app/route/Nav.js
+++ app/route/Nav.js
@@ -765,8 +765,11 @@ class Route extends React.Component {
 
   switchRoute = (prevNav, nav, action) => {
     //关闭loading显示,防止进入下一页面，上一个页面的loading显示还在
-    EasyShowLD.switchRoute();    
-    EasyToast.switchRoute();
+    if(action && action.routeName){
+      EasyShowLD.switchRoute();    
+      EasyToast.switchRoute();
+    }
+
     routeLength = nav.routes.length;
     
     //切换到个人中心，更新用户信息
