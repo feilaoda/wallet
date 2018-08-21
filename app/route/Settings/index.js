6e70ba457fb2aff4d24e001d4b1e9bc086c064eb
marco <lxgaisqq@163.com>
2018-08-21 21:53:48 +0800

    v2.3.1.2

--- app/route/Settings/index.js
+++ app/route/Settings/index.js
@@ -168,7 +168,7 @@ class Setting extends React.Component {
             <View style={styles.footer}>
               <Text style={styles.foottext}>© 2018 eostoken all rights reserved </Text>
               {/* <Text style={styles.foottext}>EOS专业版钱包 V{DeviceInfo.getVersion()}</Text> */}
-              <Text style={styles.foottext}>EOS专业版钱包 V2.3.1.1</Text>
+              <Text style={styles.foottext}>EOS专业版钱包 V2.3.1.2</Text>
             </View>
           </View>
       </ScrollView>
